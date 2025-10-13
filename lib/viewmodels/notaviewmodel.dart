3491e0e3c1cc1d5dd645ec3787b5c4790733d30a
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-10-13 19:13:03 -0300

    API

--- lib/viewmodels/notaviewmodel.dart
+++ lib/viewmodels/notaviewmodel.dart
@@ -2,23 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
 import '../models/nota.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class NotaViewModel extends ChangeNotifier {
+
+  final String url = "https://sheetdb.io/api/v1/7u6kxme44sp1k";
 
   List<Nota> _notas = [];
 
   List<Nota> get notas => List.unmodifiable(_notas);
+
+  NotaViewModel() {
+    read();
+  }
 
   void create(Nota nota) {
     nota.id = Uuid().v4();
     _notas.add(nota);
     // exemplo, para seminário:
     // firebase.collection('notas').add(nota);
+
+    http.post(
+      Uri.parse(url),
+      body: json.encode(nota.toJson()),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    );
+
     notifyListeners();
   }
 
   void delete(String id) {
     _notas.removeWhere((e) => e.id == id);
+    
+    http.delete(Uri.parse("$url/$id")).then((_) {});
+    
     notifyListeners();
   }
 
@@ -31,7 +51,23 @@ class NotaViewModel extends ChangeNotifier {
     var _nota = notas.where((e) => e.id == nota.id).first;
     _nota.title = nota.title;
 
+    http.put(
+      Uri.parse("$url/${nota.id}"),
+      body: json.encode(nota.toJson()),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    );
+
     notifyListeners();
   }
 
+  void read() {
+    http.get(Uri.parse(url)).then((response) {
+      var jsonList = json.decode(response.body) as List;
+      _notas = jsonList.map((e) => Nota.fromJson(e)).toList();
+    }).whenComplete(() {
+      notifyListeners();
+    });
+  }
 }
